43f0fc749a906cd45328c45513faf6bd0e2fa03b
Vahdet Savci <savcivahdet@gmail.com>
2023-11-02 16:07:28 +0300

    application is done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace arraylist;
 
@@ -6,6 +8,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ArrayList liste = new ArrayList();
+        // liste.Add("Ayşe");
+        // liste.Add(2);
+        // liste.Add(true);
+        // liste.Add('A');
+
+        // Console.WriteLine(liste[1]);
+
+        // foreach (var item in liste)
+        //     Console.WriteLine(item);
+
+        Console.WriteLine("***** Add Range *****");
+        // string[] renkler = {"kırmızı", "sarı", "yeşil"};
+        List<int> sayilar = new List<int>(){1,8,3,7,9,92,5};
+        // liste.AddRange(renkler);
+        liste.AddRange(sayilar);
+
+        foreach (var item in liste)
+            Console.WriteLine(item);
+
+        Console.WriteLine("***** Sort *****");
+        liste.Sort();
+
+        foreach (var item in liste)
+            Console.WriteLine(item);
+        
+        // Binary Search
+        Console.WriteLine("***** Binary Search *****");
+        Console.WriteLine(liste.BinarySearch(9));
+
+        Console.WriteLine("***** Reverse *****");
+        liste.Reverse();
+        foreach (var item in liste)
+            Console.WriteLine(item);
+
+        Console.WriteLine("***** Clear *****");
+        liste.Clear();
+
+        foreach (var item in liste)
+            Console.WriteLine(item);
     }
 }
